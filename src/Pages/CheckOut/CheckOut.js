f4ff627f8a8ac415076c2f92bd4a94b7940f4c4e
MohammadNasir520 <nasirahsan520@gmail.com>
2022-10-27 16:18:56 +0600

    checkout page responsive

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import "./CheckOut.css"
 
 const CheckOut = () => {
     const course=useLoaderData();
     const {title}=course;
     console.log(course)
     return (
-        <div>
+        <div className='premium-text-container mt-5 d-flex align-items-center flex-column justify-content-center' >
             <h3 className='text-center'>Wellcome to Premium <span className='text-info'>{title} </span> course</h3>
+            <p>Start a great Journy with us . Work hard you will definitely be success!</p>
         </div>
     );
 };
